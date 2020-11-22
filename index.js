c9eeae7ead653de89828926931a5ed7243f2d051
Mulitate4 <aryan.dongre1605@gmail.com>
2020-11-22 16:59:33 +0530

    decor is complete

---
+++ index.js
@@ -0,0 +1,8 @@
+genius = require("genius-api")
+var genius = new api(process.env.GENIUS_CLIENT_ACCESS_TOKEN);
+
+const searchButton = document.querySelector(".searchButton");
+var songInput = document.querySelector(".searchInput");
+
+getLyrics(songName){
+    genius.search(songName)
